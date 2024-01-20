06dde1e03fe212cf0f09cfea8424f6d644f22abe
jjindol <andy15948@naver.com>
2024-01-21 00:19:32 +0900

    feat: repository 작성

--- src/main/java/com/streetreview/review/repository/ReviewRepository.java
+++ src/main/java/com/streetreview/review/repository/ReviewRepository.java
@@ -1,2 +1,15 @@
-package com.streetreview.review.repository;public class ReviewRepository {
+package com.streetreview.review.repository;
+
+import com.streetreview.member.entity.Member;
+import com.streetreview.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByMember(Member member);
+
 }
