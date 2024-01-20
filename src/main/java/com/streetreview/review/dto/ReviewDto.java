7e09efbbe13fa9be2a0d8ab698500626aec5dd02
jjindol <andy15948@naver.com>
2024-01-21 00:17:51 +0900

    feat: dto 작성

--- src/main/java/com/streetreview/review/dto/ReviewDto.java
+++ src/main/java/com/streetreview/review/dto/ReviewDto.java
@@ -1,4 +1,23 @@
 package com.streetreview.review.dto;
 
-public class ReviewCreationDto {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReviewDto {
+
+    private String content;
+    private String photo;
+    private Long streetId;
+    private Long memberId;
+
+    @Builder
+    public ReviewDto(String content, String photo,Long streetId, Long memberId) {
+        this.content = content;
+        this.photo = photo;
+        this.streetId = streetId;
+        this.memberId = memberId;
+    }
 }
