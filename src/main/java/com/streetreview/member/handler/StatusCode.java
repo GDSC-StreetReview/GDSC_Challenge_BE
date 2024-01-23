48c85a626ce112c6d308235b34f8398bbd686b0c
jjindol <andy15948@naver.com>
2024-01-23 16:16:12 +0900

    feat: StatusCode 추가

--- src/main/java/com/streetreview/member/handler/StatusCode.java
+++ src/main/java/com/streetreview/member/handler/StatusCode.java
@@ -16,7 +16,8 @@ public enum StatusCode {
     DISABLED_ACCOUNT(403, "삭제된 계정입니다.", HttpStatus.FORBIDDEN),
     USERNAME_NOT_FOUND(404, "가입된 이메일이 없습니다.", HttpStatus.NOT_FOUND),
     SLACK_ERROR_TEST(400, "앞으로 에러나면 여기에 출몰", HttpStatus.BAD_REQUEST),
-    ALREADY_EXIST(400, "이미 존재하는 데이터입니다.", HttpStatus.BAD_REQUEST)
+    ALREADY_EXIST(400, "이미 존재하는 데이터입니다.", HttpStatus.BAD_REQUEST),
+    NOT_FOUND_MEMBER(404,"존재하지 않는 회원입니다.",HttpStatus.NOT_FOUND)
     ;
     @Getter
     private int statusCode;
