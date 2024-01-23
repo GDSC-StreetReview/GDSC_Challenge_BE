f96f09a3cbae8a826bdd2ac1e0ca4ffc9f2ff60f
jjindol <andy15948@naver.com>
2024-01-23 16:15:10 +0900

    fix: 회원 검색 로직 변경

--- src/main/java/com/streetreview/review/service/ReviewServiceImpl.java
+++ src/main/java/com/streetreview/review/service/ReviewServiceImpl.java
@@ -2,14 +2,13 @@ package com.streetreview.review.service;
 
 
 import com.streetreview.member.entity.Member;
+import com.streetreview.member.handler.CustomException;
+import com.streetreview.member.handler.StatusCode;
 import com.streetreview.member.repository.MemberRepository;
 import com.streetreview.review.dto.ReviewDto;
-import com.streetreview.review.entity.Review;
 import com.streetreview.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.EntityNotFoundException;
 
 
 @Service
@@ -23,19 +22,11 @@ public class ReviewServiceImpl implements ReviewService {
     public void writeReview(ReviewDto reviewDto) {
 
         //회원 검색
-        Member member = memberRepository.findById(reviewDto.getMemberId())
-                .orElseThrow(() -> new EntityNotFoundException("회원이 존재하지 않습니다."));
+        memberRepository.findById(reviewDto.getMemberId())
+                .orElseThrow(() -> new CustomException(StatusCode.NOT_FOUND_MEMBER));
 
 
-        // 리뷰 엔티티 생성
-        Review review = Review.builder()
-                .content(reviewDto.getContent())
-                .photo(reviewDto.getPhoto())
-                .member(member)
-                .build();
-
-        // 리뷰 저장
-        reviewRepository.save(review);
+        reviewRepository.save(reviewDto.toReviewEntity());
     }
 }
 
