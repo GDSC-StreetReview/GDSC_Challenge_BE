f3bae81cc5c26031715f38134062d5508e9a2810
김대현 <zxz4641@jayuroun.com>
2024-02-13 23:53:42 +0900

    a

--- src/main/java/com/streetreview/member/oauth/GoogleAuth.java
+++ src/main/java/com/streetreview/member/oauth/GoogleAuth.java
@@ -3,6 +3,9 @@ package com.streetreview.member.oauth;
 import com.streetreview.member.dto.GoogleAuthDto;
 import com.streetreview.member.dto.ResGoogleToken;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -27,17 +30,21 @@ public class GoogleAuth {
     public static final String MEMBER_INFO_URL = "https://oauth2.googleapis.com/tokeninfo";
 
     public ResGoogleToken getGoogleOauthToken(String code) {
-        //String decode = URLDecoder.decode(code, StandardCharsets.UTF_8);
+        String decode = URLDecoder.decode(code, StandardCharsets.UTF_8);
         RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("Accept", "application/json");
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         System.out.println("redirect-uri = " + REDIRECT_URI);
         params.add("code", code);
         params.add("client_id", clientId);
         params.add("client_secret", clientSecret);
-        //params.add("redirect_uri", REDIRECT_URI);
+        params.add("redirect_uri", REDIRECT_URI);
         params.add("grant_type", "authorization_code");
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
 
-        return restTemplate.postForObject(TOKEN_URL, params, ResGoogleToken.class);
+        return restTemplate.postForObject(TOKEN_URL, request, ResGoogleToken.class);
     }
 
     public GoogleAuthDto getGoogleOauthTokenInfo(String id_token) {
