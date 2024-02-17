bfb3ec7fe2d305ced58a14f3db8836286931e9c5
김대현 <zxz4641@jayuroun.com>
2024-02-17 13:53:30 +0900

    noArg

--- src/main/java/com/streetreview/review/dto/ReqStreetPointDto.java
+++ src/main/java/com/streetreview/review/dto/ReqStreetPointDto.java
@@ -3,9 +3,11 @@ package com.streetreview.review.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReqStreetPointDto {
     private Double x;
     private Double y;
